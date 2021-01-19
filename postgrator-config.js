b63d8bc47e55c03d8f51eadbaa8d1190b781e7b1
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-18 19:37:19 -0800

    editing to deploy to heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DB_URL
-    : process.env.DB_URL
+  "connectionString": 'postgres://gxqwryurhazjnz:7bf8033790cba14a418f2f6c142244a75efabbf5cba678265b6d325a732b3326@ec2-54-160-18-230.compute-1.amazonaws.com:5432/dcm486nsmd43ug'
 };
