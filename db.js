fb52ed3ca927517c79782b2600cddb7615ef9830
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-18 18:59:04 -0800

    edits

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    port: 5432,
-    database: process.env.DATABASE || 'dcm486nsmd43ug',
-    password: process.env.PASSWORD,
+    // user: process.env.USER,
+    // host: process.env.HOST,
+    // port: 5432,
+    // database: process.env.DATABASE || 'dcm486nsmd43ug',
+    // password: process.env.PASSWORD,
     URI: 'postgres://gxqwryurhazjnz:7bf8033790cba14a418f2f6c142244a75efabbf5cba678265b6d325a732b3326@ec2-54-160-18-230.compute-1.amazonaws.com:5432/dcm486nsmd43ug'
 });
 
