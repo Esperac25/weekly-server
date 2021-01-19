5f40ae58b82621ec54b52043ea43803a073cfe11
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-18 18:09:53 -0800

    updates

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
+    user: 'gxqwryurhazjnz',
+    host: 'ec2-54-160-18-230.compute-1.amazonaws.com',
     port: 5432,
-    database: 'weekly'
+    database: 'dcm486nsmd43ug',
+    password: '7bf8033790cba14a418f2f6c142244a75efabbf5cba678265b6d325a732b3326'
 });
 
 module.exports = pool;
