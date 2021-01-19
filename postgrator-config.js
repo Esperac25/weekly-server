6a5cb2ccf7954eb4e3334e3b440dcdf4a2b93e88
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-18 19:50:32 -0800

    editing for heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": 'postgres://gxqwryurhazjnz:7bf8033790cba14a418f2f6c142244a75efabbf5cba678265b6d325a732b3326@ec2-54-160-18-230.compute-1.amazonaws.com:5432/dcm486nsmd43ug'
+  "connectionString": 'postgres://hgedvcbsviqevj:4613af01a407bee5aa4e99e1e6341decd272eb6c2acc431d87ea4d8677942681@ec2-50-19-32-202.compute-1.amazonaws.com:5432/d5vqr0ims776po'
 };
