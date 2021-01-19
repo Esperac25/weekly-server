7d3b2df5c9287e38be4ff6113333d1bc07cac9ab
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-18 20:32:12 -0800

    refactoring

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": 'postgres://hgedvcbsviqevj:4613af01a407bee5aa4e99e1e6341decd272eb6c2acc431d87ea4d8677942681@ec2-50-19-32-202.compute-1.amazonaws.com:5432/d5vqr0ims776po'
+  "connectionString": process.env.DATABASE_URL || 'postgres://hgedvcbsviqevj:4613af01a407bee5aa4e99e1e6341decd272eb6c2acc431d87ea4d8677942681@ec2-50-19-32-202.compute-1.amazonaws.com:5432/d5vqr0ims776po'
 };
