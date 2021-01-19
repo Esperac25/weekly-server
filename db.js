9e7d88361496b993af5f0d65d40af38a07758ba9
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-18 18:28:03 -0800

    editing to include .env data

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'gxqwryurhazjnz',
-    host: 'ec2-54-160-18-230.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'dcm486nsmd43ug',
-    password: '7bf8033790cba14a418f2f6c142244a75efabbf5cba678265b6d325a732b3326'
+    user: process.env.USER,
+    host: process.env.HOST,
+    port: process.env.PORT || 5432,
+    database: process.env.DATABASE || 'dcm486nsmd43ug',
+    password: process.env.PASSWORD 
 });
 
 module.exports = pool;
